17fd6653ee89a076ab2d749f2f197cccb409ac3c
Karen Gonzalez <karenciriaco14@gmail.com>
2026-06-08 12:15:29 -0600

    Fix postgres timestamp

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,11 @@ async function bootstrap() {
     new ValidationPipe(),
   );
 
-  await app.listen(3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Servidor corriendo en puerto ${port}`);
 }
 
 bootstrap();
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -24,28 +24,24 @@ export class User {
   @Column()
   password!: string;
 
-  // ROLE
   @Column({
     default: 'empleado',
   })
   role!: string;
 
-  // STATUS
   @Column({
     default: 'activo',
   })
   status!: string;
 
-  // TOKEN PARA RECUPERAR CONTRASEÑA
   @Column({
     nullable: true,
   })
   resetToken!: string;
 
-  // FECHA DE EXPIRACIÓN DEL TOKEN
   @Column({
     nullable: true,
-    type: 'datetime',
+    type: 'timestamp',
   })
   resetTokenExpires!: Date;
 
